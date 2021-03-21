c5aa378fdc3554a65eb002031452ab93e03c5a92
gguaranha <sangioweb@gmail.com>
2021-03-21 12:41:25 -0300

    Criando Rotas

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,17 +1,23 @@
+import 'package:agenda_crud/app/view/contact_form.dart';
+import 'package:agenda_crud/app/view/contact_list.dart';
 import 'package:flutter/material.dart';
-
-import 'view/my_home_page.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  static const HOME = '/';
+  static const CONTACT_FORM = 'contact-form';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: MyHomePage(title: 'Aula Flutter'),
+      routes: {
+        HOME: (context) => ContactList(),
+        CONTACT_FORM: (context) => ContactForm()
+      },
     );
   }
 }
